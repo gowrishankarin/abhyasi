6eb280df2da21a6b1fbd56e10072ff1339b80c9f
Gowri Shankar <gowrishankar_m@hotmail.com>
2015-04-04 23:03:46 +0530

    View for Abhyasi index and detail

--- abhyasis/views.py
+++ abhyasis/views.py
@@ -1,16 +1,20 @@
-from django.http import HttpResponse
+from django.http import Http404
+from django.shortcuts import render
 
 from .models import Abhyasi
 
 # Create your views here.
 def index(request):
 	latest_abhyasi_list = Abhyasi.objects.order_by('-name')[:5]
-	output = ', '.join([p.name for p in latest_abhyasi_list])
-	return HttpResponse(output)
+	context = {	'latest_abhyasi_list': latest_abhyasi_list }
+	return render(request, 'abhyasis/index.html', context)
 	
 def detail(request, abhyasi_id):
-	response = "You are looking at the results of question %s"
-	return HttpResponse(response % abhyasi_id)
+	try:
+		abhyasi = Abhyasi.objects.get(pk=abhyasi_id)
+	except Abhyasi.DoesNotExist:
+		raise Http404("Abhyasi does not exist")
+	return render(request, 'abhyasis/detail.html', {'abhyasi': abhyasi})
 
 def results(request, abhyasi_id):
 	return detail(request, abhyasi_id)
