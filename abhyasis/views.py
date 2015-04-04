6dc47c78cb5ef7b521c51d9fefe2a0057c47e80d
Gowri Shankar <gowrishankar_m@hotmail.com>
2015-04-04 22:00:21 +0530

    Abhyasi name comes on abhyasis index page

--- abhyasis/views.py
+++ abhyasis/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import Abhyasi
 
 # Create your views here.
+def index(request):
+	latest_abhyasi_list = Abhyasi.objects.order_by('-name')[:5]
+	output = ', '.join([p.name for p in latest_abhyasi_list])
+	return HttpResponse(output)
+	
+def detail(request, abhyasi_id):
+	response = "You are looking at the results of question %s"
+	return HttpResponse(response % abhyasi_id)
+
+def results(request, abhyasi_id):
+	return detail(request, abhyasi_id)
--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,31 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.template import RequestContext, loader
+from django.shortcuts import get_object_or_404, render
+
+from .models import Question
 
 # Create your views here.
 def index(request):
-	return HttpResponse("Hello, world. You are at the polls index")
+	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+	template = loader.get_template('polls/index.html')
+	context = RequestContext(request, {
+		'latest_question_list': latest_question_list,
+	})
+	return HttpResponse(template.render(context))
+
+	# Following code does the same as the one inside index method
+	# latest_question_list = Question.objects.order_by('-pub_date')[:5]
+	# context = {'latest_question_list': latest_question_list}
+	# return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-	return HttpResponse("You are looking at question %s." % question_id)
+	#try:
+	#	question = Question.objects.get(pk=question_id)
+	#except Question.DoesNotExist:
+	#	raise Http404("Question does not exist")
+	#return render(request, 'polls/detail.html', {'question': question})
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
 	response = "Yur are looking at the results of question %s."
